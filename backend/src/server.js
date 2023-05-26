748fc3312a46db93d35731d8307c043c5d76cc84
Eli Cury <elicury2019@gmail.com>
2023-05-25 22:48:05 -0300

    chore: criado get e post dentro de comunicacao.

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,32 +7,62 @@ const port = 3000
 
 
 // connect to database mysql
-connection.connect()
+connection.connect();
 
-// create routes
-app.get('/', (req, res) => {
-  res.send('Hackathon!')
+// obter todos usuarios.
+app.get('/comunicacao', (req, res) => {
+	connection.query(`SELECT * FROM comunicacao as c
+	 INNER JOIN assinantes as a ON c.id = a.id_comunicacao 
+	 INNER JOIN receptores as r ON r.id = a.id_comunicacao`, (error, results) => {
+	  if (error) throw error;
+	  res.json(results);
+	});
+  });
 
-  connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-    if (err) {
-      throw err
+//adicionar uma nova comunicacao
+app.post('/comunicacao', (req, res) => {
+	const { emissor, data_criacao, assunto, mensagem, observacao, emitido_por, modo, tipo } = req.body;
+	connection.query('INSERT INTO comunicacao (emissor, data_criacao, assunto, mensagem, observacao, emitido_por, modo, tipo) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [emissor, data_criacao, assunto, mensagem, observacao, emitido_por, modo, tipo], (error, results) => {
+	  if (error) throw error;
+	  res.json({ message: 'Comunicacao Criada com Sucesso!', id: results.insertId });
+	});
+  });
+
+// obter uma comunicacao por id.
+app.get('/comunicacao/:id', (req, res) => {
+  const { id } = req.params;
+  connection.query('SELECT * FROM comunicacao WHERE id = ?', id, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.json(results[0]);
+    } else {
+      res.status(404).json({ message: "Comunicação não encontrada" });
     }
-    
-    console.log('The solution is: ', rows[0].solution)
-  })
-})
+  });
+});
 
+//atualizar uma comunicacao
+app.put('/comunicacao/:id', (req, res) => {
+	const { id } = req.params;
+	const { emissor, data_criacao, assunto, mensagem, observacao, emitido_por, modo, tipo } = req.body;
+	connection.query('UPDATE users SET emissor = ?, data_criacao = ?, assunto = ?, mensagem = ?, observacao = ?, emitido_por = ?, modo = ?, tipo = ?, id = ?', [emissor, data_criacao, assunto, mensagem, observacao, emitido_por, modo, tipo], (error) => {
+	  if (error) throw error;
+	  res.json({ message: 'Comunicação editada com Sucesso!' });
+	});
+  });
+  
+//autetenticando o login do usuario.
 app.post('/auth', function(request, response) {
-	let username = request.body.email;
-	let password = request.body.senha;
+	let email = request.body.email;
+	let senha = request.body.senha;
 
-  if (username && password) {
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+  if (email && senha) {
+		connection.query('SELECT * FROM users WHERE email = ? AND senha = ?', [email, senha], function(error, results, fields) {
 			if (error) throw error;
 
       if (results.length > 0) {
 				request.session.loggedin = true;
-				request.session.username = username;
+				request.session.email = email;
 				response.redirect('/home');
 			} else {
 				response.send('E-mail e/ou senha inválidos!');
@@ -48,3 +78,5 @@ app.post('/auth', function(request, response) {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
