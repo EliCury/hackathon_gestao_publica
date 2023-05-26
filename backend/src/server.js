1914e59750de5d23330340f713e85e99eef2bcf7
Vagner Kuntz <vaagner.kuntz@gmail.com>
2023-05-26 00:31:55 -0300

    chore: add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 
 import { connection } from './database/index.js'
 
 const app = express()
 const port = 3000
 
+app.use(cors({
+	origin: '*',
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}))
 
 // connect to database mysql
 connection.connect();
